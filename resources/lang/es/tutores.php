80a0b87c5bd3d9da6fca9bf7777050e4a327efd1
Jose Maria Samos <josemasamos@gmail.com>
2021-02-24 17:04:45 +0100

    Tarea 4: completa

---
+++ resources/lang/es/tutores.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "tipo_doc" => "Tipo de documento",
+    "nif" => "NIF",
+    "nie" => "NIE",
+    "pasaporte" => "Pasaporte",
+    "estado" => "Estado",
+    "activo" => "Activo",
+    "inactivo" => "Inactivo",
+    "anadir" => "Añadir"
+];
--- resources/views/tutor.blade.php
+++ resources/views/tutor.blade.php
@@ -17,64 +17,64 @@
                     <h1>Añadir tutor</h1>
                 </div>
                 <form action="">
-                    <label for="empresa" class="form-label">Empresa</label>
+                    <label for="empresa" class="form-label">{{ __("empresa") }}</label>
                     <input type="text" id="empresa" name="empresa" class="form-control" value="{{ old('empresa') }}"/>
-                    <div class="form-text mb-2">Nombre de la empresa asiganada</div>
+                    <div class="form-text mb-2">{{ __("empresa") }}</div>
 
-                    <label for="tipo_doc" class="form-label">Tipo de documento</label>
+                    <label for="tipo_doc" class="form-label">{{ __("tipo_doc") }}</label>
                     <select class="form-select form-select-sm" id="tipo_doc" name="tipo_doc">
-                        <option {{ old("tipo_doc") != "" ? "" : "selected" }}>Tipo de documento</option>
-                        <option value="nif" {{ old("tipo_doc") == "nif" ? "selected" : "" }}>NIF</option>
-                        <option value="nie" {{ old("tipo_doc") == "nie" ? "selected" : "" }}>NIE</option>
-                        <option value="pasaporte" {{ old("tipo_doc") == "pasaporte" ? "selected" : "" }}>Pasaporte</option>
+                        <option {{ old("tipo_doc") != "" ? "" : "selected" }}>{{ __("tutores.tipo_doc") }}</option>
+                        <option value="nif" {{ old("tipo_doc") == "nif" ? "selected" : "" }}>{{ __("tutores.nif") }}</option>
+                        <option value="nie" {{ old("tipo_doc") == "nie" ? "selected" : "" }}>{{ __("tutores.nie") }}</option>
+                        <option value="pasaporte" {{ old("tipo_doc") == "pasaporte" ? "selected" : "" }}>{{ __("tutores.pasaporte") }}</option>
                     </select>
-                    <div class="form-text mb-2">Tipo de documento de identidad</div>
+                    <div class="form-text mb-2">{{ __("tipo_doc") }}</div>
 
-                    <label for="dni" class="form-label">Documento de identidad</label>
+                    <label for="dni" class="form-label">{{ __("dni") }}</label>
                     <input type="text" id="dni" name="dni" class="form-control" value="{{ old('dni') }}"/>
-                    <div class="form-text mb-2">Tu documento de identidad</div>
+                    <div class="form-text mb-2">{{ __("dni") }}</div>
 
-                    <label for="nombre" class="form-label">Nombre</label>
+                    <label for="nombre" class="form-label">{{ __("nombre") }}</label>
                     <input type="text" id="nombre" name="nombre" class="form-control" value="{{ old('nombre') }}"/>
-                    <div class="form-text mb-2">Nombre del tutor</div>
+                    <div class="form-text mb-2">{{ __("nombre") }}</div>
 
-                    <label for="primer_apellido" class="form-label">Primer apellido</label>
+                    <label for="primer_apellido" class="form-label">{{ __("primer_apellido") }}</label>
                     <input type="text" id="primer_apellido" name="primer_apellido" class="form-control" value="{{ old('primer_apellido') }}"/>
-                    <div class="form-text mb-2">Primer apellido del tutor</div>
+                    <div class="form-text mb-2">{{ __("primer_apellido") }}</div>
 
-                    <label for="segundo_apellido" class="form-label">Segundo apellido</label>
+                    <label for="segundo_apellido" class="form-label">{{ __("segundo_apellido") }}</label>
                     <input type="text" id="segundo_apellido" name="segundo_apellido" class="form-control" value="{{ old('segundo_apellido') }}"/>
-                    <div class="form-text mb-2">Segundo apellido del tutor</div>
+                    <div class="form-text mb-2">{{ __("segundo_apellido") }}</div>
 
-                    <label for="pais_dni" class="form-label">País DNI</label>
+                    <label for="pais_dni" class="form-label">{{ __("pais_dni") }}</label>
                     <input type="text" id="pais_dni" name="pais_dni" class="form-control" value="{{ old('pais_dni') }}"/>
-                    <div class="form-text mb-2">País del documento de identidad</div>
+                    <div class="form-text mb-2">{{ __("pais_dni") }}</div>
 
-                    <label for="provincia" class="form-label">Província</label>
+                    <label for="provincia" class="form-label">{{ __("provincia") }}</label>
                     <input type="text" id="provincia" name="provincia" class="form-control" value="{{ old('provincia') }}"/>
-                    <div class="form-text mb-2">Província del tutor</div>
+                    <div class="form-text mb-2">{{ __("provincia") }}</div>
 
-                    <label for="municipio" class="form-label">Municipio</label>
+                    <label for="municipio" class="form-label">{{ __("municipio") }}</label>
                     <input type="text" id="municipio" name="municipio" class="form-control" value="{{ old('municipio') }}"/>
-                    <div class="form-text mb-2">Municipio del tutor</div>
+                    <div class="form-text mb-2">{{ __("municipio") }}</div>
 
-                    <label for="estado" class="form-label">Estado</label>
+                    <label for="estado" class="form-label">{{ __("estado") }}</label>
                     <select class="form-select form-select-sm" id="estado" name="estado">
-                        <option {{ old("tipo_doc") != "" ? "" : "selected" }}>Estado</option>
-                        <option value="activo" {{ old("estado") == "activo" ? "selected" : "" }}>Activo</option>
-                        <option value="inactivo" {{ old("estado") == "inactivo" ? "selected" : "" }}>Inactivo</option>
+                        <option {{ old("tipo_doc") != "" ? "" : "selected" }}>{{ __("tutores.estado") }}</option>
+                        <option value="activo" {{ old("estado") == "activo" ? "selected" : "" }}>{{ __("tutores.activo") }}</option>
+                        <option value="inactivo" {{ old("estado") == "inactivo" ? "selected" : "" }}>{{ __("tutores.inactivo") }}</option>
                     </select>
-                    <div class="form-text mb-2">Estado actual del tutor</div>
+                    <div class="form-text mb-2">{{ __("estado") }}</div>
                 
-                    <label for="telefono" class="form-label">Teléfono</label>
+                    <label for="telefono" class="form-label">{{ __("telefono") }}</label>
                     <input type="text" id="telefono" name="telefono" class="form-control" value="{{ old('telefono') }}"/>
-                    <div class="form-text mb-2">Teléfono del tutor</div>
+                    <div class="form-text mb-2">{{ __("telefono") }}</div>
                 
-                    <label for="email" class="form-label">Mail</label>
+                    <label for="email" class="form-label">{{ __("email") }}</label>
                     <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}"/>
-                    <div class="form-text mb-2">Correo electrónico del tutor</div>
+                    <div class="form-text mb-2">{{ __("email") }}</div>
 
-                    <button type="submit" class="btn btn-primary">Primary</button>
+                    <button type="submit" class="btn btn-primary">{{ __("tutores.anadir") }}</button>
                 </form>
             </div>
             <div class="col-3"></div>
